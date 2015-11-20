b5a753c143a77d46739a079a1cea10294d440aec
Dirk Einecke <dirkeinecke@users.noreply.github.com>
2015-11-20 05:53:49 +0100

    Update misc.js

--- assets/js/misc.js
+++ assets/js/misc.js
@@ -1,5 +1,10 @@
+function htmlEntities(str) {
+  return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
+
 function onlineDemoConvert(markdown) {
   var converter = new showdown.Converter();
   html = converter.makeHtml(markdown);
-  document.getElementById('online-demo-html-code').innerHTML = html;
+  document.getElementById('online-demo-html-code').innerHTML = htmlEntities(html);
+  document.getElementById('online-demo-output').innerHTML = html;
 }
